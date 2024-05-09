e984e3c11330848c48f2e358d36de4ef7bd9208c
Lucas <soareslucas134@gmail.com>
2024-05-09 08:17:20 -0300

    Inicialização do projeto

---
+++ src/main/java/com/trilharapp/projeto_trilhar_back_end/ProjetoTrilharBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.trilharapp.projeto_trilhar_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTrilharBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTrilharBackEndApplication.class, args);
+	}
+
+}
